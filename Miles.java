904a48a3ad883bd66085d17a9177d96cae62a38a
Alina Lizyaeva <l_alin@mail.ru>
2020-06-20 23:07:00 +0600

    Initial commit

---
+++ Miles.java
@@ -0,0 +1,7 @@
+public class Miles {
+    public static void main(String[] args) {
+        long ticketPrice = 3999;
+        long amountMiles = ticketPrice / 20;
+        System.out.println(amountMiles);
+    }
+}
