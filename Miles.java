921c30418a7ebe994313533b60d2c6584bfba45f
Alina Lizyaeva <l_alin@mail.ru>
2020-06-24 22:53:19 +0600

    Update

--- Miles.java
+++ Miles.java
@@ -1,7 +1,7 @@
 public class Miles {
     public static void main(String[] args) {
-        long ticketPrice = 3999;
-        long amountMiles = ticketPrice / 20;
+        int ticketPrice = 2999;
+        int amountMiles = ticketPrice / 20;
         System.out.println(amountMiles);
     }
 }
